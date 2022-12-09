bbb071cd3a3e9398236117a57edd2f5221958a8e
shekhus <cpsatav@gmail.com>
2022-12-10 00:03:10 +0530

    create structure

---
+++ Components/index.js
@@ -0,0 +1,8 @@
+import HeroSection from './HeroSection/HeroSection';
+import Model from './Model/Model';
+import NavBar from './Model/Model';
+import SearchToken from './SearchToken/SearchToken';
+import Toggle from './Toggle/Toggle';
+import TokenList from './TokenList';
+
+export {HeroSection,Model,NavBar,SearchToken,Toggle,TokenList};
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => 
+<div>
+<Component {...pageProps} />
+</div>
 
 export default MyApp
